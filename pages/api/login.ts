0ee2ff2ad61d2fb35e977db75403f5a2ae456968
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-11-04 19:28:15 +0000

    cc

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
+import { supabase } from '../../utils/supabase';
 type Data = {
   error: string;
 };
@@ -8,13 +8,18 @@ interface Error {
   message: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method === 'POST') {
     try {
       const { email, password } = req.body;
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      console.log('data, error', data, error);
       // ...log in the user
     } catch (error) {
       res.status(500).json({ error: error.message });
