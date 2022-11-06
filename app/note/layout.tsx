6c6731c89e9dad743a9759115e6ef6484b37cf48
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-11-06 17:19:31 +0000

    cc

--- app/note/layout.tsx
+++ app/note/layout.tsx
@@ -4,16 +4,21 @@ import { supabase } from '../../utils/supabaseclient';
 interface LoginProps {
   children: ReactNode;
 }
+const mynotes = async () => {
+  const { data: notes, error } = await supabase.from('notes').select('id');
+  console.log('noteds', notes);
+  return { notes, error };
+};
 
 export default async function LoginLayout({ children }: LoginProps) {
-  const { data: notes, error } = await supabase.from('notes').select('*');
+  const { notes, error } = mynotes();
 
   return (
     <section className="flex flex-row">
       <div className="flex flex-col">
         {error && <div>{error?.message}</div>}
         {notes?.map((note) => (
-          <p>{note}</p>
+          <p>{note.id}</p>
         ))}
       </div>
       {children}
