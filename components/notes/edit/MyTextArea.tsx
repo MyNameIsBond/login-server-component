b6061192a808a16734b8177a33544ed959de9e94
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-11-17 17:03:05 +0000

    cc

--- components/notes/edit/MyTextArea.tsx
+++ components/notes/edit/MyTextArea.tsx
@@ -8,10 +8,10 @@ export default function MyTextArea({ data }: { data: string }) {
   useEffect(() => {}, [value]);
 
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row min-w-fit">
       <textarea
         onChange={(e) => setValue(e.target.value)}
-        className="w-full h-full"
+        className="w-full h-full dark:bg-neutral-900 dark:text-gray-50 text-gray-900"
         value={value}
       />
       <MdPreview value={value} />
