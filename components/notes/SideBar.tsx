072d5350e2d9a29123321e297ce8bf0221e6585e
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-11-28 18:53:09 +0000

    cc

--- components/notes/SideBar.tsx
+++ components/notes/SideBar.tsx
@@ -1,18 +1,35 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SideNotes from './SideNotes';
 import Search from './Search';
 import SearchNotes from './SearchNotes';
+import { supabase } from '../../utils/supabaseclient';
 
 export default function SideBar({}: {}) {
   const [search, setSearch] = useState<string>('');
+  const [notes, setNotes] = useState<any>([]);
+
+  // listen to notes changes
+  useEffect(() => {
+    const notes = supabase
+      .channel('custom-insert-channel')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'notes' },
+        (payload) => {
+          console.log('Change received!', payload);
+          setNotes(payload.new);
+        }
+      )
+      .subscribe();
+  }, [notes]);
 
   return (
     <div className="flex flex-col border-r dark:border-neutral-50 dark:border-opacity-10 bg-gray-50 dark:bg-neutral-900 dark:text-neutral-100 sticky top-0 overflow-y-scroll h-screen w-96">
       <div className="border-b border-gray-900 dark:border-gray-50 dark:border-opacity-10 border-opacity-10 p-2 flex sticky top-0 dark:bg-neutral-900 dark:bg-opacity-5 backdrop-blur-md">
         <Search search={search} setSearch={setSearch} />
       </div>
-      {search ? <SearchNotes search={search} /> : <SideNotes search={search} />}
+      {search ? <SearchNotes search={search} /> : <SideNotes notes={notes} />}
     </div>
   );
 }
--- components/notes/SideNotes.tsx
+++ components/notes/SideNotes.tsx
@@ -1,15 +1,6 @@
-import { use } from 'react';
 import Note from './Note';
 
-const fetcher = async () => {
-  const data = await fetch('http://localhost:3000/api/notes');
-  return await data.json();
-};
-
-const datafech = fetcher();
-
-export default function SideNotes() {
-  const notes = use(datafech);
+export default function SideNotes({ notes }) {
   return (
     <div className="p-5 flex flex-col gap-y-5">
       {notes?.map((note: any) => (
